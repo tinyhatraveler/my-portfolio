8ebaf174569687899c9a39fef9e2900e55b35ad5
luca hawes <lucahawes@gmail.com>
2026-06-05 14:19:02 +0100

    added the knowledge base

---
+++ knowledge.js
@@ -0,0 +1,63 @@
+const tips = [
+  "basic html",
+  "basic git",
+  "custom email domain setup",
+  "advanced knowledge about iphone X-17",
+  "tf2 modding",
+  "tf2 server hosting",
+  "how to make a website",
+  "how to make a discord bot",
+  "how to make a youtube channel",
+  "how to make a twitter account",
+  "how to make a reddit account",
+  "how to use ios",
+  "how to use the apple shorcuts app",
+  "create areonautics",
+  "create mod",
+  "minecraft",
+  "redstone",
+  "very basic python",
+  "extensive photoshop knowledge",
+  "how to procrastinate",
+  "extensive knowledge of feminising hrt",
+  "the art of hiding",
+  "how to stop people from noticing stuff",
+  "how to use android",
+  "how to install and use linux",
+  "how to use mac os",
+  "how to use windows",
+  "how dialup internet works",
+  "current political climate of america",
+  "current political climate of the UK",
+  "the entire script for meet the spy",
+  "fortnite",
+  "stormworks",
+  "how to use repitition well",
+  "how to use repitition well",
+  "how to pull off a propeller hat",
+];
+
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+const list = document.getElementById("knowledge-list");
+const search = document.getElementById("search");
+
+function renderList(filter = "") {
+  list.innerHTML = "";
+  tips
+    .filter(tip => tip.toLowerCase().includes(filter.toLowerCase()))
+    .forEach(tip => {
+      const li = document.createElement("li");
+      li.textContent = tip;
+      list.appendChild(li);
+    });
+}
+
+search.addEventListener("input", () => {
+  renderList(search.value);
+});
+
+shuffle(tips);
+renderList();
